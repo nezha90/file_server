88ec3ded87a7abfa5620c9bba89c65601b44d2f9
nezha <448688985@qq.com>
2024-03-11 12:12:06 +0800

    file server

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var (
+	ConfigSource string
+	WebPort      int
+	WebPath      string
+)
+
+func init() {
+	flag.StringVar(&ConfigSource, "c", "default", "config source default or env.")
+	flag.IntVar(&WebPort, "port", 5002, "web port.")
+	flag.StringVar(&WebPath, "path", "./dist", "web path.")
+	flag.Parse()
+
+	if ConfigSource == "env" {
+		WebPort = getEnvInt("WEB_PORT", WebPort)
+		WebPath = getEnvString("WEB_PATH", WebPath)
+	}
+}
+
+func main() {
+
+	fmt.Printf("File Server Port:%d Path:%s", WebPort, WebPath)
+
+	http.Handle("/", http.FileServer(http.Dir(WebPath)))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", WebPort), nil)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+}
+
+func getEnvString(name string, value string) string {
+	ret := os.Getenv(name)
+	if ret == "" {
+		return value
+	} else {
+		return ret
+	}
+}
+
+func getEnvInt(name string, value int) int {
+	env := os.Getenv(name)
+	if ret, err := strconv.Atoi(env); env == "" || err != nil {
+		return value
+	} else {
+		return ret
+	}
+}
